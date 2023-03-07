919813a2e32f5694593018d322dc683e16551e0a
Haobo Chen <chenhaob@grinnell.edu>
2023-03-06 22:16:33 -0600

    lab01

--- lab01/exercise2/pwd_checker.c
+++ lab01/exercise2/pwd_checker.c
@@ -23,13 +23,13 @@ For the simplicity of this exercise:
 /* Returns true if the length of PASSWORD is at least 10, false otherwise */
 bool check_length(const char *password) {
     int length = strlen(password);
-    bool meets_len_req = (length <= 10);
+    bool meets_len_req = (length >= 10);
     return meets_len_req;
 }
 
 /* Returns true if LETTER is in the range [LOWER, UPPER], false otherwise */
 bool check_range(char letter, char lower, char upper) {
-    bool is_in_range = (letter > lower && letter < upper);
+    bool is_in_range = (letter >= lower && letter <= upper);
     return is_in_range;
 }
 
@@ -60,7 +60,7 @@ bool check_lower(const char *password) {
 /* Returns true if PASSWORD contains at least one number, false otherwise */
 bool check_number(const char *password) {
     while (*password != '\0') {
-        if (check_range(*password, 0, 9)) {
+        if (check_range(*password, '0', '9')) {
             return true;
         }
         ++password;
@@ -83,14 +83,9 @@ bool check_name(const char *first_name, const char *last_name, const char *passw
 bool check_password(const char *first_name, const char *last_name, const char *password) {
     bool length, upper, lower, number, name;
     lower = check_lower(password);
-    assert(lower);
     length = check_length(password);
-    assert(length);
     name = check_name(first_name, last_name, password);
-    assert(check_name);
     number = check_number(password);
-    assert(number);
     upper = check_upper(password);
-    assert(upper);
     return (lower && length && name && upper && number);
 }
--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,22 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+    while (fast_ptr != NULL) {
+        fast_ptr = fast_ptr->next;
+        if (fast_ptr == NULL) {
+            return 0;
+        }
+        fast_ptr = fast_ptr->next;
+        if (fast_ptr == NULL) {
+            return 0;
+        }
+        slow_ptr = slow_ptr->next;
+        if (fast_ptr == slow_ptr) {
+            return 1;
+        }
+
+    }
+    return 0;
 }
