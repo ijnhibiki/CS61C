8108cf8b7e05d6b63a5625aad4fea51b1498d868
Haobo Chen <chenhaob@grinnell.edu>
2023-04-03 10:10:05 -0500

    test

--- proj4/tests/unittests/unittests.py
+++ proj4/tests/unittests/unittests.py
@@ -14,6 +14,7 @@ class TestAdd(TestCase):
         dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 2, seed=0)
         dp_mat2, nc_mat2 = rand_dp_nc_matrix(2, 2, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
+    
         self.assertTrue(is_correct)
         print_speedup(speed_up)
 
