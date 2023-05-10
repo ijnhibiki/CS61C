d75a40abab86efb43e768415e7872809837d474e
Haobo Chen <chenhaob@grinnell.edu>
2023-05-10 08:12:50 -0500

    Update write_matrix.s

--- proj2/src/write_matrix.s
+++ proj2/src/write_matrix.s
@@ -25,45 +25,45 @@
 write_matrix:
 
     # Prologue
-    addi sp, sp, -20
-    sw ra, 0(sp)
-    sw a0, 4(sp)
-    sw a1, 8(sp)
-    sw a2, 12(sp)
-    sw a3, 16(sp)
+    addi sp, sp, -20                 # Allocate stack
+    sw ra, 0(sp)                     # Store return address to stack
+    sw a0, 4(sp)                     # Store arguments to stack
+    sw a1, 8(sp)                     # Store arguments to stack
+    sw a2, 12(sp)                    # Store arguments to stack
+    sw a3, 16(sp)                    # Store arguments to stack
     #Call fopen
-    li a1, 1
-    jal fopen
-    li t0, -1
-    bne a0, t0, open_success
-    li a0, 27
-    lw ra, 0(sp)
-    addi sp, sp, 16
-    j exit
+    li a1, 1                         # Store 1 to a1
+    jal fopen                        # Call fopen function
+    li t0, -1                        # Store -1 to t0
+    bne a0, t0, open_success         # Check if fopen is successful, if so, jump to open_success
+    li a0, 27                        # Store error code to a0
+    lw ra, 0(sp)                     # Load return address to ra
+    addi sp, sp, 16                  # Deallocate stack
+    j exit                           # Jump to exit to terminate program
     
 open_success:
-    sw a0, 4(sp)             #Store file descriptor to stack
-    li a0, 4
-    jal malloc
-    bne a0, x0, malloc_success_1
-    lw ra, 0(sp)
-    addi sp, sp, 16
-    li a0, 26
-    j exit
+    sw a0, 4(sp)                     # Store file descriptor to stack
+    li a0, 4                         # Store 4 to a0
+    jal malloc                       # Call malloc function
+    bne a0, x0, malloc_success_1     # Check if malloc is successful, if so, jump to malloc_success_1
+    lw ra, 0(sp)                     # If malloc is not successful, load return address to ra
+    addi sp, sp, 16                  # Deallocate stack
+    li a0, 26                        # Store error code to a0
+    j exit                           # Jump to exit to terminate program
     
 malloc_success_1:
-    add a1, a0, x0
-    lw t0, 12(sp)
-    sw t0, 0(a1)
-    lw a0, 4(sp)
-    li a2, 1
-    li a3, 4
-    jal fwrite
-    li t0, 1
-    beq a0, t0, write_row_success
-    li a0, 30
-    addi sp, sp, 20
-    j exit
+    add a1, a0, x0                   # Store the address of the allocated memory to a1
+    lw t0, 12(sp)                    # Load the number of rows from stack 
+    sw t0, 0(a1)                     # Store the number of rows to stack
+    lw a0, 4(sp)                     # Load file descriptor from stack
+    li a2, 1                         # Store 1 to a2 as inital value
+    li a3, 4                         # Store 4 to a3 as inital value
+    jal fwrite                       # Call fwrite function
+    li t0, 1                         # Store 1 to t0
+    beq a0, t0, write_row_success    # Check if fwrite is successful, if so, jump to write_row_success
+    li a0, 30                        # Store error code to a0
+    addi sp, sp, 20                  # Deallocate stack
+    j exit                           # Jump to exit to terminate program
     
 write_row_success:
     li a0, 4
