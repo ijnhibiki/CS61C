670ecfef3dbf125d96ff57c58ade8bb71e7259a0
Haobo Chen <chenhaob@grinnell.edu>
2023-05-20 22:35:46 -0500

    Create copy.sh

---
+++ proj4/copy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp /usr/local/lib/python3.6/dist-packages/dumbpy-0.0.1.egg-info ~/sp23-s303/proj4/.venv/lib/python3.6/site-packages
+cp /usr/local/lib/python3.6/dist-packages/numc-0.0.1.egg-info ~/sp23-s303/proj4/.venv/lib/python3.6/site-packages
+cp /usr/local/lib/python3.6/dist-packages/dumbpy.cpython-36m-x86_64-linux-gnu.so ~/sp23-s303/proj4/.venv/lib/python3.6/site-packages
+cp /usr/local/lib/python3.6/dist-packages/numc.cpython-36m-x86_64-linux-gnu.so ~/sp23-s303/proj4/.venv/lib/python3.6/site-packages
