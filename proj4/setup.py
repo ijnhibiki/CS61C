63acc5aed334b8f7af8d1884ea465e8542c18f51
Haobo Chen <chenhaob@grinnell.edu>
2023-10-22 00:36:14 -0500

    Update setup.py

--- proj4/setup.py
+++ proj4/setup.py
@@ -2,31 +2,25 @@ from distutils.core import setup, Extension
 import sysconfig
 
 def main():
-    # CFLAGS = ['-g', '-Wall', '-Wno-unused-function', '-Wconversion', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
-    # LDFLAGS = ['-fopenmp']
-    # setup(name="numc",
-    #       version="0.0.1",
-    #       description="numc matrix operations",
-    #       ext_modules=[
-    #           Extension("numc",
-    #                     sources=["src/numc.c", "src/matrix.c"],
-    #                     extra_compile_args=CFLAGS,
-    #                     extra_link_args=LDFLAGS,
-    #                     language='c')
-    #       ])
     CFLAGS = ['-g', '-Wall', '-Wno-unused-function', '-Wconversion', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
     LDFLAGS = ['-fopenmp']
-    setup(name="dumbpy",
-          version="0.0.1",
-          description="dumbpy matrix operations",
-          ext_modules=[
-            Extension("dumbpy",
-                      sources=["src/dumbpy.c", "src/dumbpy_matrix.c"],
-                      extra_compile_args=CFLAGS,
-                      extra_link_args=LDFLAGS,
-                      language='c')
-          ])
 
+    numc_module = Extension("numc",
+                            sources=["src/numc.c", "src/matrix.c"],
+                            extra_compile_args=CFLAGS,
+                            extra_link_args=LDFLAGS,
+                            language='c')
+
+    dumbpy_module = Extension("dumbpy",
+                              sources=["src/dumbpy.c", "src/dumbpy_matrix.c"],
+                              extra_compile_args=CFLAGS,
+                              extra_link_args=LDFLAGS,
+                              language='c')
+
+    setup(name="MyPackages",
+          version="0.0.1",
+          description="Numc and Dumbpy matrix operations",
+          ext_modules=[numc_module, dumbpy_module])
 
 if __name__ == "__main__":
     main()
