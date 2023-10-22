e9144515c8884a4e72bdf55c76c387076ea8922c
Haobo Chen <chenhaob@grinnell.edu>
2023-10-22 00:34:10 -0500

    Update setup.py

--- proj4/setup.py
+++ proj4/setup.py
@@ -2,14 +2,26 @@ from distutils.core import setup, Extension
 import sysconfig
 
 def main():
+    # CFLAGS = ['-g', '-Wall', '-Wno-unused-function', '-Wconversion', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
+    # LDFLAGS = ['-fopenmp']
+    # setup(name="numc",
+    #       version="0.0.1",
+    #       description="numc matrix operations",
+    #       ext_modules=[
+    #           Extension("numc",
+    #                     sources=["src/numc.c", "src/matrix.c"],
+    #                     extra_compile_args=CFLAGS,
+    #                     extra_link_args=LDFLAGS,
+    #                     language='c')
+    #       ])
     CFLAGS = ['-g', '-Wall', '-Wno-unused-function', '-Wconversion', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
     LDFLAGS = ['-fopenmp']
-    setup(name="numc",
+    setup(name="dumbpy",
           version="0.0.1",
-          description="numc matrix operations",
+          description="dumbpy matrix operations",
           ext_modules=[
-            Extension("numc",
-                      sources=["src/numc.c", "src/matrix.c"],
+            Extension("dumbpy",
+                      sources=["src/dumbpy.c", "src/dumbpy_matrix.c"],
                       extra_compile_args=CFLAGS,
                       extra_link_args=LDFLAGS,
                       language='c')
