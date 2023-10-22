fdbf708b3b339f05d5d5f7c87f6811cede46c7c4
Haobo Chen <chenhaob@grinnell.edu>
2023-10-22 16:37:03 -0500

    Update unittests.py

--- proj4/tests/unittests/unittests.py
+++ proj4/tests/unittests/unittests.py
@@ -25,8 +25,8 @@ class TestAdd(TestCase):
         print_speedup(speed_up)
 
     def test_large_add(self):
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1000, 1000, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1000, 1000, seed=1)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(100000, 100000, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(100000, 100000, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "add")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
